7abf98a359a3151a791a9decd12b92dca7785dae
vasanth1999developer <vasumalar1920@gmail.com>
2025-03-17 18:12:18 +0530

    Add files via upload

---
+++ account-service/src/main/java/com/microservices/account/entity/BaseVO.java
@@ -0,0 +1,22 @@
+package com.microservices.account.entity;
+
+import java.io.Serializable;
+
+import java.util.Date;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+
+@MappedSuperclass
+@Data
+public class BaseVO implements Serializable {
+private static final long serialVersionUID = -5074230273317960920L;
+
+    
+    private long createdBy;
+    private Date created = new Date();
+    private Date modified = new Date();
+    private long modifiedBy;
+
+}
